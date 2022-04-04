5a41d59d124be5392af9380f196018218878e73f
Boengiu <darius.boengiu@capgemini.com>
2022-04-04 08:49:30 +0300

    Update

--- src/main/java/com/capgemini/coursesmanagementsystem/controller/StudentController.java
+++ src/main/java/com/capgemini/coursesmanagementsystem/controller/StudentController.java
@@ -6,10 +6,7 @@ import com.capgemini.coursesmanagementsystem.service.CourseService;
 import com.capgemini.coursesmanagementsystem.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/students")
@@ -24,5 +21,15 @@ public class StudentController {
     @PostMapping("/")
     public ResponseEntity<StudentDTO> add(@RequestBody StudentDTO studentDTO) {
         return new ResponseEntity<>(studentService.addStudent(studentDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentDTO> getById(@PathVariable Integer id) {
+        return new ResponseEntity<>(studentService.getStudentById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("{student_id}/course/{course_id}")
+    public void enrollStudent(@PathVariable("student_id") Integer studentId, @PathVariable("course_id") Integer courseId) {
+        studentService.enrollStudent(studentId, courseId);
     }
 }
--- src/main/java/com/capgemini/coursesmanagementsystem/service/StudentService.java
+++ src/main/java/com/capgemini/coursesmanagementsystem/service/StudentService.java
@@ -2,9 +2,7 @@ package com.capgemini.coursesmanagementsystem.service;
 
 import com.capgemini.coursesmanagementsystem.entity.Course;
 import com.capgemini.coursesmanagementsystem.entity.Student;
-import com.capgemini.coursesmanagementsystem.entity.dto.CourseDTO;
 import com.capgemini.coursesmanagementsystem.entity.dto.StudentDTO;
-import com.capgemini.coursesmanagementsystem.mapper.CourseMapper;
 import com.capgemini.coursesmanagementsystem.mapper.StudentMapper;
 import com.capgemini.coursesmanagementsystem.repository.CourseRepository;
 import com.capgemini.coursesmanagementsystem.repository.StudentRepository;
@@ -14,13 +12,37 @@ import org.springframework.stereotype.Service;
 public class StudentService {
 
     private final StudentRepository studentRepository;
+    private final CourseRepository courseRepository;
+//    private final Student student;
+//    private final Course course;
 
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository, CourseRepository courseRepository) {
         this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
+//        this.student = student;
+//        this.course = course;
     }
 
     public StudentDTO addStudent(StudentDTO studentDTO) {
         Student newStudent = studentRepository.save(StudentMapper.toEntity(studentDTO));
         return StudentMapper.toDTO(newStudent);
+    }
+
+    public StudentDTO getStudentById(Integer studentId) {
+        Student foundStudent = studentRepository.getById(studentId);
+        return StudentMapper.toDTO(foundStudent);
+    }
+
+    public void enrollStudent(Integer studentId, Integer courseId) {
+        Course course = new Course();
+        Student student = new Student();
+
+        if (studentRepository.findById(studentId).isPresent() && courseRepository.findById(courseId).isPresent()) {
+            student = studentRepository.findById(studentId).get();
+            course = courseRepository.findById(courseId).get();
+        }
+
+        student.getCourses().add(course);
+        course.getStudents().add(student);
     }
 }
